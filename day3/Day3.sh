cd5635bd29d44c8acb0a296292aed456cdb202f1
“Viishah” <vtorsalle@gmail.com>
2023-02-28 21:03:43 +0530

    Add: Day3 practice problm is added

---
+++ day3/Day3.sh
@@ -0,0 +1,10 @@
+
+
+      for files in *.txt
+do
+	foldername=`echo $files|awk -F. '{print $1}'`;
+	echo $folderName;
+	echo $files;
+	datetime=$(date +"%m_%d_%y");
+	echo $folderName$datetime.log;
+done
