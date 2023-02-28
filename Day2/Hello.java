0394541c306cd5928d5a98b56e0b527265781ed5
“Viishah” <vtorsalle@gmail.com>
2023-02-28 20:55:25 +0530

    Add: Day2 practice problm is added

---
+++ Day2/Hello.java
@@ -0,0 +1,10 @@
+class Hello {
+	public static void main(String[] args) {
+		while(true)
+		{
+			System.out.println("Hello World!!!");
+			try {
+				Thread.sleep(5000);
+			} catch(Exception e)
+			}
+		
